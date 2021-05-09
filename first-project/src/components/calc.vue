54c73054ffcb279e4d299a2470e4fcb08fe7075d
Ivan Borozinetc <46967249+Despe573@users.noreply.github.com>
2021-05-10 00:05:41 +0300

    final update lesson1

--- first-project/src/components/calc.vue
+++ first-project/src/components/calc.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
     <div class="display">
-      <input v-model="operand1" />
-      <input v-model="operand2" />
+      <input v-model.number="operand1" />
+      <input v-model.number="operand2" />
       = {{ result }}
     </div>
     <div class="keyboard">
-      <button @click="result = operand1 + operand2">+</button>
-      <button @click="result = operand1 - operand2">-</button>
-      <button @click="result = operand1 / operand2">/</button>
-      <button @click="result = operand1 * operand2">*</button>
+      <button value="sum" @click="calculate($event)">+</button>
+      <button value="sub" @click="calculate($event)">-</button>
+      <button value="div" @click="calculate($event)">/</button>
+      <button value="mul" @click="calculate($event)">*</button>
+      <button value="int" @click="calculate($event)">%</button>
+      <button value="exp" @click="calculate($event)">**</button>
     </div>
   </div>
 </template>
@@ -23,6 +25,30 @@ export default {
       operand2: 0,
       result: 0,
     };
+  },
+  methods: {
+    calculate(event) {
+      switch (event.target.value) {
+        case "sum":
+          this.result = this.operand1 + this.operand2;
+          break;
+        case "sub":
+          this.result = this.operand1 - this.operand2;
+          break;
+        case "div":
+          this.result = this.operand1 / this.operand2;
+          break;
+        case "mul":
+          this.result = this.operand1 * this.operand2;
+          break;
+        case "int":
+          this.result = this.operand1 % this.operand2;
+          break;
+        case "exp":
+          this.result = this.operand1 ** this.operand2;
+          break;
+      }
+    },
   },
 };
 </script>
